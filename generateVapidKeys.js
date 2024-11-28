4ffae111cc7a676e3808b1e32d1c76764998e560
365risk <4everyonedream@gmail.com>
2024-11-28 22:15:52 +0530

    Updates

--- generateVapidKeys.js
+++
@@ -1,7 +0,0 @@
-// generateVapidKeys.js
-const webpush = require('web-push');
-
-// Generate VAPID keys
-const vapidKeys = webpush.generateVAPIDKeys();
-console.log('Public Key:', vapidKeys.publicKey);
-console.log('Private Key:', vapidKeys.privateKey);
